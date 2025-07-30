69785f324414e71e11cc9e4b8a8fab9493b19959
Quentin Adam <35724429+QuentinAd@users.noreply.github.com>
2025-07-30 02:02:22 -0600

    Fix Airflow compose config

--- data-pipeline/dags/etl_job.py
+++ data-pipeline/dags/etl_job.py
@@ -1,7 +1,10 @@
 from airflow import DAG
+import os
+
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import (
     KubernetesPodOperator,
 )
+from airflow.providers.docker.operators.docker import DockerOperator
 from datetime import datetime
 
 default_args = {
@@ -14,16 +17,27 @@ with DAG(
     schedule_interval=None,
     catchup=False,
     default_args=default_args,
-    description="Run PySpark ETL in KubernetesPodOperator",
+    description="Run PySpark ETL",
 ) as dag:
-    run_spark = KubernetesPodOperator(
-        task_id="run_spark_job",
-        name="spark-etl-job",
-        namespace="default",
-        image="271111372751.dkr.ecr.ca-central-1.amazonaws.com/spark-etl:latest",
-        cmds=["/opt/bitnami/spark/bin/spark-submit"],
-        arguments=["/app/etl_job.py"],
-        get_logs=True,
-        is_delete_operator_pod=True,
-        service_account_name="spark-runner",
-    )
+    if os.getenv("LOCAL_AIRFLOW", "false").lower() == "true":
+        run_spark = DockerOperator(
+            task_id="run_spark_job",
+            image="spark-etl:local",
+            auto_remove=True,
+            command="/opt/bitnami/spark/bin/spark-submit /app/etl_job.py",
+            docker_url="unix://var/run/docker.sock",
+            network_mode="bridge",
+        )
+    else:
+        run_spark = KubernetesPodOperator(
+            task_id="run_spark_job",
+            name="spark-etl-job",
+            namespace="default",
+            image="271111372751.dkr.ecr.ca-central-1.amazonaws.com/spark-etl:latest",
+            cmds=["/opt/bitnami/spark/bin/spark-submit"],
+            arguments=["/app/etl_job.py"],
+            get_logs=True,
+            is_delete_operator_pod=True,
+            service_account_name="spark-runner",
+        )
+
