c6a84499eebad3b7105f33a7aa01642c3dc2ad36
QuentinAd <qadam2602@gmail.com>
2025-10-21 11:27:00 -0600

    feat: add hirag third party package

---
+++ third_party/hhy-huang_hirag/eval/insert_context_openai.py
@@ -0,0 +1,19 @@
+import os
+import json
+from hirag import HiRAG
+
+os.environ["OPENAI_API_KEY"] = "***"
+
+DATASET = "mix"
+file_path = f"./datasets/{DATASET}/{DATASET}_unique_contexts.json"
+
+graph_func = HiRAG(
+    working_dir=f"./datasets/{DATASET}/work_dir_hi",
+    enable_hierachical_mode=True,
+    embedding_func_max_async=4,
+    enable_naive_rag=True,
+)
+
+with open(file_path, mode="r") as f:
+    unique_contexts = json.load(f)
+    graph_func.insert(unique_contexts)
